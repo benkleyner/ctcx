29297291756db6c2be2d5427ff0840c524f034e3
Benjamin Kleyner <54718380+benkleyner@users.noreply.github.com>
2026-07-10 12:41:20 -0700

    Add conditional filesystem rules (#5)

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub mod compiler;
 pub mod model;
 
 pub use compiler::{
-    BuildSafety, Check, CompiledProject, DriftReport, ExplainStatus, PathKind, Project,
+    BuildSafety, Check, CompiledProject, Condition, DriftReport, ExplainStatus, PathKind, Project,
     build_project, check_project, compile_project, discover_config, explain_rule, explain_target,
     init_project, load_project, render_diffs,
 };
